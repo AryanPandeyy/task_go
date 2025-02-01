b39c60aff267f9cd92820e00f00cc352eac2f0d2
Aryan Pandey <margiaryan@gmail.com>
2025-02-01 16:49:30 +0530

    roughly done with add

--- main.go
+++ main.go
@@ -1,51 +1,79 @@
 package main
 
 import (
-	"fmt"
 	"encoding/json"
+	"fmt"
 	"log"
-	// "io"
-	// "io/fs"
 	"os"
+	"time"
 )
 
+type ListTask struct {
+	Tasks []Task
+}
+
 type Task struct {
-	Id			int
-	Description	string
-	Status		string
-	CreatedAt	int
-	UpdatedAt	int
+	Id          int
+	Description string
+	Status      string
+	CreatedAt   string
+	UpdatedAt   string
 }
 
 func main() {
-	var cmd string
-	fmt.Scan(&cmd)
-	if cmd == "add" {
+	if len(os.Args) < 2 {
+		log.Fatal("Please provide argument")
+	}
+	if os.Args[1] == "add" {
+		f, err := os.OpenFile("/home/ap/code/fun/tp/go/sample", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
 		var tsk string
 		fmt.Print("Enter the task: ")
 		fmt.Scan(&tsk)
-		t1 := Task{1, tsk, "NOTDONE", 1, 1}
-		b, e := json.Marshal(t1)
+		t := Task{1, tsk, "NOTDONE", time.Now().Format(time.DateTime), time.Now().Format(time.DateTime)}
+		fmt.Println(t)
+		// 		t1 := ListTask{Tasks: []Task{t}}
+		//
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		b, e := os.ReadFile("/home/ap/code/fun/tp/go/sample")
 		if e != nil {
 			log.Fatal(e)
 		}
-		fmt.Println(string(b))
+		fmt.Println(b[0])
 
-		f,err := os.OpenFile("/home/ap/code/fun/tp/go/sample", os.O_RDWR|os.O_CREATE, 0644);
-		if  err != nil {
-			log.Fatal(err)
+		tmp := ListTask{Tasks: []Task{}}
+		json.Unmarshal(b, &tmp)
+		fmt.Print("EH", tmp.Tasks[1])
+		if (len(tmp.Tasks) > 1) {
+			tmp.Tasks = append(tmp.Tasks, t)
+
+			b, e := json.MarshalIndent(tmp.Tasks, "", "  ")
+			if e != nil {
+				log.Fatal(e)
+			}
+			fmt.Println(string(b))
+
+			if _,err := f.Write(b); err != nil {
+				log.Fatal(err)
+			}
 		}
 
-		if _,err := f.WriteString(string(b)); err != nil {
-			log.Fatal(err)
-		}
+		// b, e := json.MarshalIndent(t1, "", "  ")
+		// if e != nil {
+		// 	log.Fatal(e)
+		// }
+		// fmt.Println(string(b))
 
-		// err := os.WriteFile("/home/ap/code/fun/tp/go/sample", b, 0644);
-		// if  err != nil {
+		// if _,err := f.Write(b); err != nil {
 		// 	log.Fatal(err)
 		// }
 
 		f.Close()
-	} else if cmd == "list" {
+	} else if os.Args[1] == "list" {
 	}
 }
